31acb8cab3b3abdc9dce75808d5c2443baaf6307
mahita <mahita.bvrm@gmail.com>
2024-10-18 22:29:48 +0530

    updated files

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -1,8 +1,10 @@
 
 import './App.css'
+import DepartmentComponent from './components/DepartmentComponent'
 import EmployeeComponent from './components/EmployeeComponent'
 import FooterComponent from './components/FooterComponent'
 import HeaderComponent from './components/HeaderComponent'
+import ListDepartmentComponent from './components/ListDepartmentComponent'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
@@ -20,8 +22,14 @@ function App() {
         <Route path='/employees' element={ <ListEmployeeComponent />}></Route>
         {/* //http://localhost:3000/add-employee */}
         <Route path='/add-employee' element={ <EmployeeComponent />}></Route>
-        {/* //http://localhost:3000/edit-employee/1 */}
+        {/* //http://localhost:3000/edit-employee/1  */}
         <Route path='/edit-employee/:id' element={<EmployeeComponent/>}></Route>
+        {/* //http://localhost:3000/departments  */}
+        <Route path='/departments' element={<ListDepartmentComponent />} ></Route>
+        {/* //http://localhost:3000/add-department  */}
+        <Route path='/add-department' element={<DepartmentComponent/>}></Route>
+
+        <Route path='/edit-department/:id' element={<DepartmentComponent />}></Route>
 
       </Routes>
       <FooterComponent/>
